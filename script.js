bf8af1c766c59b1dda484c0b04356aa55742c7ca
Kahmyl <kamilhabib556@gmail.com>
2023-04-23 20:02:06 +0100

    Made UI refactors

---
+++ script.js
@@ -0,0 +1,7 @@
+function bookTicket() {
+  window.open("https://forms.gle/7xbtCVyiExVu3VKr5", "_blank");
+}
+
+function joinWhatsApp() {
+  window.open("https://chat.whatsapp.com/HlR8Uxvm5mc9kUy9pbnpQV", "_blank");
+}
